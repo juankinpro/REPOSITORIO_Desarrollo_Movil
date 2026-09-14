ec36aa940f4138a2ba68bcf1e4ff8ef1da4e0099
Juan <juan.lozada696@pascualbravo.edu.co>
2026-09-13 20:42:52 -0500

    Update Taller_fundamentos_dart.dart

---
+++ Taller_fundamentos_dart.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nombre = "Juan Lozada";
+  int edad = 24;
+  double promedio = 3.5;
+  bool matriculado = true;
+
+  print("Nombre: $nombre");
+  print("Edad: $edad");
+  print("Promedio: $promedio");
+  print("Matriculado: $matriculado");
+}
